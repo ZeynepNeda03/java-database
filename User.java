25a8d030477c037fd9437ab7bb48fa49d0614b8e
Zeynep Neda Uğuzdağ <zynpndg@gmail.com>
2025-07-31 16:22:28 +0300

    Initial commit

---
+++ User.java
@@ -0,0 +1,37 @@
+
+class User {
+
+    int id;
+    String username;
+    String email;
+
+    User(String username, int id, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
